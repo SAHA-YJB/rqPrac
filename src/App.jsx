469317cde2de847a3736330be74ce40e4942e947
SAHA-YJB <pkomom123@naver.com>
2023-11-20 18:05:26 +0900

    features: react-router with react-query use

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ import {
 import Events from "./components/Events/Events.jsx";
 import EventDetails from "./components/Events/EventDetails.jsx";
 import NewEvent from "./components/Events/NewEvent.jsx";
-import EditEvent from "./components/Events/EditEvent.jsx";
+import EditEvent, {
+  loader as editEventLoader,
+  action as editEventAction,
+} from "./components/Events/EditEvent.jsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../src/util/http.js";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -34,6 +37,8 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/events/:id/edit",
+        loader: editEventLoader,
+        action: editEventAction,
         element: <EditEvent />,
       },
     ],
--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,45 +1,55 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
+import {
+  Link,
+  redirect,
+  useNavigate,
+  useNavigation,
+  useParams,
+  useSubmit,
+} from "react-router-dom";
 
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { fetchEvent, queryClient, updateEvent } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
-import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
 
 export default function EditEvent() {
   const navigate = useNavigate();
   const { id } = useParams();
+  const submit = useSubmit();
+  const { state } = useNavigation();
 
-  const { data, isPending, isError, error } = useQuery({
+  const { data, isError, error } = useQuery({
     queryKey: ["events", id],
     queryFn: ({ signal }) => fetchEvent({ signal, id }),
+    staleTime: 10000,
   });
 
-  const { mutate } = useMutation({
-    mutationFn: updateEvent,
-    //optimistic update
-    onMutate: async ({ event }) => {
-      await queryClient.cancelQueries({ queryKey: ["events", id] });
-      const previousEvent = queryClient.getQueryData(["events", id]);
+  // const { mutate } = useMutation({
+  //   mutationFn: updateEvent,
+  //   //optimistic update
+  //   onMutate: async ({ event }) => {
+  //     await queryClient.cancelQueries({ queryKey: ["events", id] });
+  //     const previousEvent = queryClient.getQueryData(["events", id]);
 
-      queryClient.setQueryData(["events", id], event);
+  //     queryClient.setQueryData(["events", id], event);
 
-      return { previousEvent }; //onError의 context로 전달
-    },
-    onError: (err, context) => {
-      queryClient.setQueryData(["events", id], context.previousEvent);
-    },
-    //성공여부와 상관없이 뮤테이션펑션이 완료될때마다 호출
-    onSettled: () => {
-      queryClient.invalidateQueries(["events", id]);
-    },
-  });
+  //     return { previousEvent }; //onError의 context로 전달
+  //   },
+  //   onError: (err, context) => {
+  //     queryClient.setQueryData(["events", id], context.previousEvent);
+  //   },
+  //   //성공여부와 상관없이 뮤테이션펑션이 완료될때마다 호출
+  //   onSettled: () => {
+  //     queryClient.invalidateQueries(["events", id]);
+  //   },
+  // });
 
   function handleSubmit(formData) {
     //onMutate로 인수를 전해준다
-    mutate({ id, event: formData });
-    navigate("../");
+    // mutate({ id, event: formData });
+    // navigate("../");
+    submit(formData, { method: "PUT" });
   }
 
   function handleClose() {
@@ -47,14 +57,6 @@ export default function EditEvent() {
   }
 
   let content;
-
-  if (isPending) {
-    content = (
-      <div className="center">
-        <LoadingIndicator />
-      </div>
-    );
-  }
 
   if (isError) {
     content = (
@@ -74,14 +76,35 @@ export default function EditEvent() {
   if (data) {
     content = (
       <EventForm inputData={data} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
+        {state === "submitting" ? (
+          "저장중..."
+        ) : (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Update
+            </button>
+          </>
+        )}
       </EventForm>
     );
   }
   return <Modal onClose={handleClose}>{content}</Modal>;
+}
+
+export function loader({ params }) {
+  return queryClient.fetchQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+  });
+}
+
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  const updatedEventData = Object.fromEntries(formData);
+  await updateEvent({ id: params.id, event: updatedEventData });
+  queryClient.invalidateQueries(["events", params.id]);
+  return redirect("../");
 }
